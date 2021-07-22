ed374b7ce280dd828bdfa71e3904150d6c553622
ternence.wu <1261747905@qq.com>
2021-07-22 09:15:48 +0800

    test

--- src/alert/demo/basic.tsx
+++ src/alert/demo/basic.tsx
@@ -4,10 +4,10 @@
  * @Github: https://github.com/TernenceWu0702
  * @Date: 2021-07-21 14:54:13
  * @LastEditors: T.W
- * @LastEditTime: 2021-07-21 15:02:06
+ * @LastEditTime: 2021-07-22 09:15:33
  */
 import React from 'react';
 import Alert from '../';
 import '../style';
 
-export default () => <Alert kind="warning">这是一条警告提示</Alert>;
+export default () => <Alert kind="warning">这是一条11警告提示</Alert>;
